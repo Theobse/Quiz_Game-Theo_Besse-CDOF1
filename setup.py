349c692234f3c524db918682b8cb07109daea749
Theobse <116637028+Theobse@users.noreply.github.com>
2024-01-22 14:56:59 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Quiz_Game-Theo_Besse-CDOF1',
+    version='1.0',
+    author='Théo Besse',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
